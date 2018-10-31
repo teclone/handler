270cf1deef9fa476a11e8de3c0b4dd42f2ea9642
Harrison Ifeanyichukwu <Harrisonifeanyichukwu@gmail.com>
2018-10-31 21:19:14 +0100

    test: create a TypesMethod helper class that returns all validation method interfaces currently avia

---
+++ tests/Helpers/DataProviders/TypesMethods.js
@@ -0,0 +1,54 @@
+export default function() {
+    return [
+        //text validator
+        'validateText',
+
+        // date validator
+        'validateDate',
+
+        //integer validation methods
+        'validateInteger',
+        'validatePInteger',
+        'validateNInteger',
+
+        //number validation methods
+        'validateFloat',
+        'validatePFloat',
+        'validateNFloat',
+
+        //email validation
+        'validateEmail',
+
+        //url validation
+        'validateURL',
+
+        //choice validation
+        'validateChoice',
+
+        //range validation
+        'validateRange',
+
+        //file validation
+        'validateFile',
+
+        //image file validation
+        'validateImage',
+
+        //audio file validation
+        'validateAudio',
+
+        //video file validation
+        'validateVideo',
+
+        //media file validation
+        'validateMedia',
+
+        //document file validation
+        'validateDocument',
+
+        'validateArchive',
+
+        //password validation
+        'validatePassword'
+    ];
+}
