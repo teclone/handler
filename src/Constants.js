b223b1a2c54e89ae41a2527b6d21e89ebf70ae6d
Harrison Ifeanyichukwu <Harrisonifeanyichukwu@gmail.com>
2018-11-05 15:13:08 +0100

    feat: create project class contants, export db model and db model case style constants

---
+++ src/Constants.js
@@ -0,0 +1,15 @@
+/**
+ * db model constants
+*/
+export const DB_MODELS = {
+    NOSQL: 1,
+    RELATIONAL: 2
+};
+
+/**
+ * supported db model case styles
+*/
+export const DB_MODEL_CASE_STYLES = {
+    CAMEL_CASE: 1,
+    SNAKE_CASE: 2
+};
