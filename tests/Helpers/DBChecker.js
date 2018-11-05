6b1f0f7a655158c5cdf60b02130b1ec1a2862e4c
Harrison Ifeanyichukwu <Harrisonifeanyichukwu@gmail.com>
2018-11-05 15:30:10 +0100

    test: create a concrete class implementation of DBChecker for testing

---
+++ tests/Helpers/DBChecker.js
@@ -0,0 +1,11 @@
+import DBCheckerAbstract from '../../src/Abstracts/DBCheckerAbstract';
+
+export default class extends DBCheckerAbstract {
+    constructor(errorBag, dbModel) {
+        super(errorBag, dbModel);
+    }
+
+    async execute() {
+
+    }
+}
