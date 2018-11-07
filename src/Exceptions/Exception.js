391a11859f2d8fcc643c0a98d5b465c963374b8b
Harrison Ifeanyichukwu <Harrisonifeanyichukwu@gmail.com>
2018-11-07 13:04:12 +0100

    build: configure babel to target latest node.js version

--- src/Exceptions/Exception.js
+++ src/Exceptions/Exception.js
@@ -1,13 +1,14 @@
 /**
  * generic Exception base class
 */
-export default class Exception {
+export default class Exception extends Error {
 
     /**
      *@param {string} [message=''] - the error message
      *@param {Object} [domain=Exception] - the error class
     */
     constructor(message, domain) {
+        super(message);
 
         const construct = (domain || Exception).prototype.constructor;
         this.message = message;
@@ -16,5 +17,3 @@ export default class Exception {
         Error.captureStackTrace(this);
     }
 }
-
-Exception.prototype = new Error();
